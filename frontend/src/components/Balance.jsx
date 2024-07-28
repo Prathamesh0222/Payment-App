63b06195a0f2a620ed3b609d2a285ef2e034135f
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2024-07-28 23:29:23 +0530

    Fetch random balance

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -1,9 +1,9 @@
 export const Balance = ({value}) => {
-    return <div className="flex">
-        <div className="font-bold text-lg">
+    return <div>
+        <div className="font-bold text-xl">
             Your balance
         </div>
-        <div className="font-semibold ml-4 text-lg" >
+        <div className="font-semibold text-2xl" >
             Rs {value}
         </div>
     </div>
